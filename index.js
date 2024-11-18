33fcbd6b67eb5dc9a07708faf5ec63b1d62ef721
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-11-18 20:39:41 +0530

    maps in js

---
+++ index.js
@@ -0,0 +1,7 @@
+ let=arr=[1,2,3,4,5,6,7]
+ let x=arr.map(function(a){return Math.pow(a,2)})
+ console.log(x);//[1,4,9,16,25,36,49]
+
+let arr1=["kumar","mahesh","vinay"]
+let y=arr1.map(b=>b.toUpperCase())
+console.log(y);//[ 'KUMAR', 'MAHESH', 'VINAY' ]
